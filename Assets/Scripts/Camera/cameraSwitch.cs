8e0d2f09a113f957b5ec370f41d239aff2375555
Irlan Farias <80763521+IrlanAndrade@users.noreply.github.com>
2024-12-10 18:28:10 -0300

    Mini Projeto 3 - Luz e Textura

--- Assets/Scripts/Camera/cameraSwitch.cs
+++ Assets/Scripts/Camera/cameraSwitch.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 
 public class cameraSwitch : MonoBehaviour
 {
-    public Camera cameraDefault1;
-    public Camera cameraFOV1;
-    public Camera cameraDefault2;
-    public Camera cameraFOV2;
+    [SerializeField]private Camera cameraDefault1;
+    [SerializeField]private Camera cameraFOV1;
+    [SerializeField]private Camera cameraDefault2;
+    [SerializeField]private Camera cameraFOV2;
+    [SerializeField]private int _actualPlayer; 
+
+    #region gets e sets
+    public int actualPlayer
+    {
+        get { return _actualPlayer; }
+        set { _actualPlayer = value; }
+    }
+
+    #endregion
 
     void Start()
     {
@@ -21,18 +31,23 @@ public class cameraSwitch : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1)){
             cameraDefault1.enabled = true;
             cameraFOV1.enabled = false;
+            actualPlayer = 1;
         }
         if (Input.GetKeyDown(KeyCode.Alpha2)){
             cameraDefault1.enabled = false;
             cameraFOV1.enabled = true;
+            
+            actualPlayer = 1;
         }
         if (Input.GetKeyDown(KeyCode.Alpha0)){
             cameraDefault2.enabled = false;
             cameraFOV2.enabled = true;
+            actualPlayer = 2;
         }
         if (Input.GetKeyDown(KeyCode.Alpha9)){
             cameraDefault2.enabled = true;
             cameraFOV2.enabled = false;
+            actualPlayer = 2;
         }
     }
 }
--- Assets/Scripts/SpaceShip/spaceshipMovement.cs
+++ Assets/Scripts/SpaceShip/spaceshipMovement.cs
@@ -2,75 +2,37 @@ using UnityEngine;
 
 public class AcceleratedMovement : MonoBehaviour
 {
-    public float maxSpeed = 10f; // Velocidade máxima do objeto
-    public float acceleration = 5f; // Taxa de aceleração
-    public float deceleration = 8f; // Taxa de desaceleração
-    private Vector3 velocity = Vector3.zero; // Velocidade atual do objeto
-    public int player;
+    #region variables
 
-    void keyboardMovement()
+    [SerializeField]private float maxSpeed = 10f;
+    [SerializeField]private float acceleration = 5f;
+    [SerializeField]private float deceleration = 8f;
+    [SerializeField]private int _player;
+    [SerializeField]private GameObject cameraController;
+    private cameraSwitch cs;
+    private Vector3 velocity = Vector3.zero; 
+
+    #endregion
+
+    #region gets e sets
+
+    public int player
     {
-        float inputHorizontal = 0;
-        float inputVertical = 0;
-
-        if (player == 1){
-            inputVertical = -Input.GetAxis("Vertical 1");
-            inputHorizontal = Input.GetAxis("Horizontal 1");
-        }
-        if (player == 2){
-            inputVertical = -Input.GetAxis("Vertical 2");
-            inputHorizontal = Input.GetAxis("Horizontal 2");
-        }
-
-        transform.Rotate(0, inputHorizontal, 0, Space.Self);
-
-        // Calcula a direção do movimento com base nas entradas
-        Vector3 direction = transform.up * inputVertical; // Move na direção que a nave está apontando
-
-        // Se há entrada, aumenta a velocidade na direção do movimento
-        if (direction.magnitude > 0)
-        {
-            velocity += direction * acceleration * Time.deltaTime;
-
-            // Limita a velocidade máxima
-            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
-        }
-        else
-        {
-            // Sem entrada: reduz gradualmente a velocidade até parar
-            velocity = Vector3.Lerp(velocity, Vector3.zero, deceleration * Time.deltaTime);
-        }
-
-        // Move o objeto com base na velocidade calculada
-        transform.Translate(velocity * Time.deltaTime, Space.World);
-
+        get { return _player; }
+        set { _player = value; }
     }
-    void mouseMovement()
-    {
-        // Captura o movimento do mouse
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
 
-        transform.Rotate(0, 0, mouseX, Space.Self);
-        transform.Rotate(-mouseY, 0, 0, Space.Self);
-    }
-    bool IsMouseInsideScreen()
-    {
-        // Obtém a posição do mouse em pixels na tela
-        Vector3 mousePosition = Input.mousePosition;
-
-        // Verifica se a posição do mouse está dentro dos limites da tela
-        return mousePosition.x >= 0 && mousePosition.x <= Screen.width &&
-               mousePosition.y >= 0 && mousePosition.y <= Screen.height;
-    }
+    #endregion
 
     void Start()
     {
-        
     }
 
     void Update()
     {   
+        
+        cs = cameraController.GetComponent<cameraSwitch>();
+
         if (IsMouseInsideScreen() == true)
         {
             mouseMovement();   
@@ -85,6 +47,57 @@ public class AcceleratedMovement : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
+    }
+    void keyboardMovement()
+    {
+        float inputHorizontal = 0;
+        float inputVertical   = 0;
+
+        if (_player == 1){
+            inputVertical   = -Input.GetAxis("Vertical 1");
+            inputHorizontal = Input.GetAxis("Horizontal 1");
+        }
+
+        if (_player == 2){
+            inputVertical   = -Input.GetAxis("Vertical 2");
+            inputHorizontal = Input.GetAxis("Horizontal 2");
+        }
+
+        transform.Rotate(0, inputHorizontal, 0, Space.Self);
+
+        Vector3 direction = transform.up * inputVertical;
+
+        if (direction.magnitude > 0)
+        {
+            velocity += direction * acceleration * Time.deltaTime;
+            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        }
+        else
+        {
+            velocity = Vector3.Lerp(velocity, Vector3.zero, deceleration * Time.deltaTime);
+        }
+        transform.Translate(velocity * Time.deltaTime, Space.World);
+
+    }
+    void mouseMovement()
+    {
+        // Captura o movimento do mouse
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        if (player == cs.actualPlayer){
+            transform.Rotate(0, 0, mouseX, Space.Self);
+            transform.Rotate(-mouseY, 0, 0, Space.Self);
+        }
+    }
+    bool IsMouseInsideScreen()
+    {
+        // Obtém a posição do mouse em pixels na tela
+        Vector3 mousePosition = Input.mousePosition;
+
+        // Verifica se a posição do mouse está dentro dos limites da tela
+        return mousePosition.x >= 0 && mousePosition.x <= Screen.width &&
+               mousePosition.y >= 0 && mousePosition.y <= Screen.height;
     }
 
 }
